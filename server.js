1d3100494a315c733637859af3e11e6d340dcdaf
Ali Moosa <m.3li.moosa@gmail.com>
2025-09-14 06:23:05 +0300

    all 4 task finshed

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const { name } = require('ejs');
 const express=require('express');
 const app=express();
 const morgan =require('morgan');
 require('dotenv').config();
+
 
 //mideelware 
 app.use(morgan('dev'));
@@ -13,26 +15,89 @@ app.get('/home',(req,res)=>{
 });
 
 //embedded link 
-const inventory = [
-  { name: 'Candle', qty: 4 },
-  { name: 'Cheese', qty: 10 },
-  { name: 'Phone', qty: 1 },
-  { name: 'Tent', qty: 0 },
-  { name: 'Torch', qty: 5 }
-];
+const RESTAURANT = {
+  name: 'The Green Byte Bistro',
+  isOpen: true,
+  address: '742 Evergreen Rd, Mapleview, OS 45502',
+  phone: '555-321-9876',
+  menu: [
+    { 
+      id: 1,
+      name: 'Quantum Quinoa Mushroom Burger',
+      price: 13.00,
+      rating: 4,
+      category: 'mains',
+      details: 'A vegetarian burger made with a quinoa and mushroom patty, it will take you to another realm.'
+    },
+    { 
+      id: 2,
+      name: 'Binary Berry Cheesecake',
+      price: 10.11,
+      rating: 3,
+      category: 'desserts',
+      details: 'A creamy cheesecake bursting with flavor. A mix of berries in every byte.'
+    },
+    { 
+      id: 3,
+      name: 'Recursive Rigatoni',
+      price: 17.00,
+      rating: 5,
+      category: 'mains',
+      details: 'A classic rigatoni pasta dish, layered with rich tomato sauce and herbs. You\'ll keep coming back for more.'
+    },
+    { 
+      id: 4,
+      name: 'Pumpkin Pi Squared',
+      price: 3.14,
+      rating: 5,
+      category: 'desserts',
+      details: 'A delightful pumpkin dessert, squared and spiced to perfection.'
+    },
+    { 
+      id: 5,
+      name: 'Fibonacci String Bean Fries',
+      price: 11.23,
+      rating: 5,
+      category: 'sides',
+      details: 'Crispy and lightly seasoned string bean fries, served in a pattern for a fun twist.'
+    }
+  ]
+}
+
 
 app.get('/', (req, res) => {
     res.render('home.ejs', {
-        inventory: inventory
+      RESTAURANT
+    
     });
 });
+app.get('/menu',(req,res)=>{
+  res.render('menu.ejs',{
+    menuu:RESTAURANT.menu,
+    
+  })
+
+})
+
+app.get('/menu/:category',(req,res)=>{
+  const category = req.params.category;
+  const menuItem =RESTAURANT.menu.filter(item=>item.category===category);
+  res.render('category.ejs',{
+    menuItem,
+    category
+  })
+})
+
 app.get('/:itemId', (req, res) => {
   const index = req.params.itemId;
-  // render show.ejs, and pass it a single item object 
   res.render('show.ejs', {
-    item: inventory[index]
+    item: RESTAURANT[index]
   });
 });
+
+
+
+
 
 
 app.listen(3000,()=>{
